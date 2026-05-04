43f137dfad453d6aecf00e6ae58eb3a4db74ded4
dodo <dodopitik4949@gmail.com>
2026-05-04 22:07:44 +0700

    Force Android Java and Kotlin JVM target 17

--- android-listener/app/build.gradle.kts
+++ android-listener/app/build.gradle.kts
@@ -24,3 +24,15 @@ android {
 kotlin {
     jvmToolchain(17)
 }
+
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+    options.release.set(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
